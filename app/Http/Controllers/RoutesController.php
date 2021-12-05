493d7597fbbf72b58792b0c74a37e6cd9bfe2dc0
Анна Бушэ <annbushe@yandex.ru>
2021-12-05 11:51:57 +0300

    Add saving at route info

--- app/Http/Controllers/RoutesController.php
+++ app/Http/Controllers/RoutesController.php
@@ -27,41 +27,46 @@ class RoutesController extends Controller
         // dd(DB::table('cities')->get());
         $data = $request->json()->all();
         $routes = DB::table('cities')
-                    ->where('city', '=', $data['city'])
-                    ->join('routes', 'cities.id', '=', 'routes.city_id')
-                    ->join('users', 'routes.user_id', '=', 'users.id')
-                    ->join('sights', 'sights.route_id', '=', 'routes.id')
-                    ->where('sights.priority', '=', '1')
-                    ->select('routes.*', 'sights.latitude', 'sights.longitude', 
-                                'users.first_name', 'users.last_name', 
-                                'users.description as user_description')
-                    ->get()->toArray();
+            ->where('city', '=', $data['city'])
+            ->join('routes', 'cities.id', '=', 'routes.city_id')
+            ->join('users', 'routes.user_id', '=', 'users.id')
+            ->join('sights', 'sights.route_id', '=', 'routes.id')
+            ->where('sights.priority', '=', '1')
+            ->select(
+                'routes.*',
+                'sights.latitude',
+                'sights.longitude',
+                'users.first_name',
+                'users.last_name',
+                'users.description as user_description'
+            )
+            ->get()->toArray();
 
         $sinLat = sin(deg2rad(floatval($data['latitude'])));
         $cosLat = cos(deg2rad(floatval($data['latitude'])));
 
-        usort($routes, function($a, $b) use ($sinLat, $cosLat, $data) {
+        usort($routes, function ($a, $b) use ($sinLat, $cosLat, $data) {
 
-            return acos($sinLat * sin(deg2rad(floatval($a->latitude))) + $cosLat * cos(deg2rad(floatval($a->latitude))) 
-                * cos(deg2rad(floatval($data['longitude']))-deg2rad(floatval($a->longitude)))) <
-                acos($sinLat * sin(deg2rad(floatval($b->latitude))) + $cosLat * cos(deg2rad(floatval($b->latitude))) 
-                * cos(deg2rad(floatval($data['longitude']))-deg2rad(floatval($b->longitude))));
+            return acos($sinLat * sin(deg2rad(floatval($a->latitude))) + $cosLat * cos(deg2rad(floatval($a->latitude)))
+                * cos(deg2rad(floatval($data['longitude'])) - deg2rad(floatval($a->longitude)))) <
+                acos($sinLat * sin(deg2rad(floatval($b->latitude))) + $cosLat * cos(deg2rad(floatval($b->latitude)))
+                    * cos(deg2rad(floatval($data['longitude'])) - deg2rad(floatval($b->longitude))));
         });
 
         $routes = array_slice($routes, $skip, $limit);
 
-        $routes = array_map( function($item) {
+        $routes = array_map(function ($item) {
             return (object) array(
-                "id"=> $item->id,
-                "name"=> $item->name,
-                "description"=> $item->description,
-                "length"=> $item->length,
-                "transport"=> $item->transport,
-                "language"=> $item->language,
-                "photo"=> $item->photo,
-                "author"=> (object) array(
-                    "name"=> $item->first_name . ' ' . $item->last_name,
-                    "description"=> $item->user_description
+                "id" => $item->id,
+                "name" => $item->name,
+                "description" => $item->description,
+                "length" => $item->length,
+                "transport" => $item->transport,
+                "language" => $item->language,
+                "photo" => $item->photo,
+                "author" => (object) array(
+                    "name" => $item->first_name . ' ' . $item->last_name,
+                    "description" => $item->user_description
                 ),
             );
         }, $routes);
@@ -71,20 +76,28 @@ class RoutesController extends Controller
 
     public function create(Request $request)
     {
-
         $request->validate([
             'name' => 'required|max:255',
+            'mainImage' => 'nullable|image|mimes:jpeg,png,jpg,gif,svg|max:2048',
         ]);
-    
+
         $route = new Route();
         $route['name'] = $request->input('name');
         $route['description'] = $request->input('description');
         $route['transport'] = $request->input('transport');
 
+        if ($request->hasFile('mainImage')) {
+            $image = $request->file('mainImage');
+            $name = time().'.'.$image->getClientOriginalExtension();
+            $destinationPath = public_path('/storage/routes/');
+            $image->move($destinationPath, $name);
+            $route['photo'] = 'storage/routes/' . $name;
+        }
+
         //get user id and default city here
         $route['user_id'] = 1;
         $route['city_id'] = 1;
-        
+
         $route->save();
 
         error_log($route);
@@ -126,30 +139,30 @@ class RoutesController extends Controller
         $trip_id = $request->input('trip_id');
         $order = $request->input('order');
         $length = $request->input('length');
-    
+
         if ($request->input('action') == 'prev') {
-                return $this->showPlace($trip_id, $order-1, $length);
-        } 
-        
+            return $this->showPlace($trip_id, $order - 1, $length);
+        }
+
         if ($request->input('action') ==  'next') {
-            return $this->showPlace($trip_id, $order+1, $length);
+            return $this->showPlace($trip_id, $order + 1, $length);
         }
 
         $request->validate([
             'name' => 'required|max:255',
-            'latitude' => ['required','regex:/^[-]?(([0-8]?[0-9])\.(\d+))|(90(\.0+)?)$/'], 
-            'longitude' => ['required','regex:/^[-]?((((1[0-7][0-9])|([0-9]?[0-9]))\.(\d+))|180(\.0+)?)$/']
+            'latitude' => ['required', 'regex:/^[-]?(([0-8]?[0-9])\.(\d+))|(90(\.0+)?)$/'],
+            'longitude' => ['required', 'regex:/^[-]?((((1[0-7][0-9])|([0-9]?[0-9]))\.(\d+))|180(\.0+)?)$/']
         ]);
 
         $place = Sight::where([
             ['priority', '=', $order],
             ['route_id', '=', $trip_id]
         ])->first();
-    
+
         if ($place == null) {
             $place = new Sight();
         }
-    
+
         $place['name'] = $request->input('name');
         $place['description'] = $request->input('description');
         $place['latitude'] = $request->input('latitude');
@@ -158,9 +171,9 @@ class RoutesController extends Controller
         $place['route_id'] = $trip_id;
 
         $place->save();
-        
+
         if ($request->input('action') == 'new') {
-            return $this->showPlace($trip_id, $length+1, $length+1);
+            return $this->showPlace($trip_id, $length + 1, $length + 1);
         }
         return $this->showTripInfo($trip_id);
     }
@@ -171,12 +184,13 @@ class RoutesController extends Controller
         $name = $route['name'];
         $description = $route['description'];
         $option = $route['transport'];
-        $length = Sight::where([['route_id','=', $trip_id]])->count();
-    
+        $length = Sight::where([['route_id', '=', $trip_id]])->count();
+
         return view("trip/overview", compact('length', 'name', 'description', 'option'));
     }
 
-    public function repopulate(Request $request) {
+    public function repopulate(Request $request)
+    {
         $id = $request->old('trip_id');
         $order = $request->old('order');
         $length = $request->old('length');
